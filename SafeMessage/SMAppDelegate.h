b69d7676e7a1bff948e102da991fc0bb9c4d7fb6
Younduk Nam <youndukn1@gmail.com>
2014-04-02 16:22:21 +0900

    Initial Commit

---
+++ SafeMessage/SMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMAppDelegate.h
+//  SafeMessage
+//
+//  Created by Younduk Nam on 4/2/14.
+//  Copyright (c) 2014 Younduk Nam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SafeMessageTests/SafeMessageTests.m
@@ -0,0 +1,34 @@
+//
+//  SafeMessageTests.m
+//  SafeMessageTests
+//
+//  Created by Younduk Nam on 4/2/14.
+//  Copyright (c) 2014 Younduk Nam. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SafeMessageTests : XCTestCase
+
+@end
+
+@implementation SafeMessageTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
